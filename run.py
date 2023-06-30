6a58ad234a812113091d87cb972fe2e03863285e
jasminabihel <>
2023-06-30 13:41:26 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def user_name():
+    user_name = input()
+    print( "Hello" + user_name )
+
+user_name()
+main()
