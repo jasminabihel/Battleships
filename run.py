821bfceac7f0115edc90b2fcfb0a080274f2bca8
jasminabihel <>
2023-07-02 06:02:31 +0000

    Add playing area and fix start game

--- run.py
+++ run.py
@@ -1,20 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-
-def user_name():
-    user_name = input()
-    print( "Hello" + user_name )
-
-user_name()
-
 def start_game():
     '''Starts the game, gets input from computer and user for move and adds a next move'''
-    pass
+    print("Welcome to the best ever Battleship game!!! \n")
+    print("The grid is 5x5 and there a total of 5 ships that take up one square each")
+    print("The objective is to destroy your opponents ships before they destroy yours! \n")
+    print("------------------------------------------------------ \n")
+
+    user = input("Enter your user name: ")
+    print("Good luck " + user)
+start_game()
 
 def create_grid():
     '''Creates the playing area for both the players,computer and user'''
-    pass
+    grid = []
+    for r in range(rows):
+        row = []
+        for c in range(columns):
+            row.append(False)
+        grid.append(row)
+    return grid
 
 def auto_add_ships():
     '''Add ships to the playing area with random number '''
