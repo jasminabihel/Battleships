29291e1742040ee403c3d659c04e33c57aad47ff
jasminabihel <>
2023-07-02 05:47:06 +0000

    Add Basic structure to Battleship

--- run.py
+++ run.py
@@ -7,4 +7,23 @@ def user_name():
     print( "Hello" + user_name )
 
 user_name()
-main()
+
+def start_game():
+    '''Starts the game, gets input from computer and user for move and adds a next move'''
+    pass
+
+def create_grid():
+    '''Creates the playing area for both the players,computer and user'''
+    pass
+
+def auto_add_ships():
+    '''Add ships to the playing area with random number '''
+    pass
+
+def score():
+    ''' Tracks the score of the players'''
+    pass
+def end_game():
+    ''' The game ends when either the player or the computer have zero battleships '''
+    pass
+
