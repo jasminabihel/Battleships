7f8cfa8153f33a799b7a42eac4f3e3305633fce2
Jasmina Bihel <jasmina.bihel@gmail.com>
2023-07-02 10:33:29 +0000

    Add updates to functons and fix problems

--- run.py
+++ run.py
@@ -5,13 +5,13 @@ import random
 class State:
     """Creates an instance of game variables"""
 
-    def __init__(self, user_score, user_ships, com_ships, ships):
-        self.user_score = user_score
-        self.user_ships = user_ships
-        self.computer_ships = computer_ships
-        #self.rows = rows
-        #self.columns = columns
+    def __init__(self, ships, rows, cols):
+        self.user_score = 0
         self.ships = ships
+        self.user_ships = self.ships
+        self.com_ships = self.ships
+        self.rows = rows
+        self.columns = cols
 
 
 def auto_add_ships(grid, state):
@@ -40,6 +40,9 @@ def create_grid(state):
 def get_move():
     """Get user input for move"""
     move = input("Whats your next target? (row 2 and column 3 = '2 3')\n")
+    if move is "":
+        print("Invalid move, try again\n")
+        return get_move()
     return move
 
 
@@ -49,7 +52,9 @@ def shoot(grid, move, is_user, state):
         splitted = move.split()
         row = int(splitted[0]) - 1
         col = int(splitted[1]) - 1
-        if row < 0 or row > 4 and col < 0 or col > 4:
+        max_row = state.rows - 1
+        max_col = state.columns - 1
+        if row < 0 or row > max_row and col < 0 or col > max_col:
             print("Invalid move \n")
             move = get_move()
             return shoot(grid, move, is_user, state)
@@ -58,14 +63,14 @@ def shoot(grid, move, is_user, state):
             print("HIT!!!\n -------")
             if is_user:
                 state.user_score += 1
-                state.computer_ships -= 1
+                state.com_ships -= 1
             else:
                 state.user_ships -= 1
         else:
             print("Missed shot\n")
 
-        print(f"Remaining user ships: ,{state.user_ships}")
-        print(f"Remaining computer ships: , {state.computer_ships}")
+        print(f"Remaining user ships: {state.user_ships}")
+        print(f"Remaining computer ships: {state.com_ships}")
         return state
 
     except AttributeError:
@@ -78,7 +83,7 @@ def end_game(state):
     """Ends game when the users ships get to zero"""
     if state.user_ships == 0:
         print("The computer won this round...\n")
-        print("Your score was " + state.user_score)
+        print(f"Your score was {state.user_score}")
         print("Better luck next time\n")
     else:
         print("Congratulations!! You won!!!")
@@ -88,15 +93,15 @@ def end_game(state):
 def start_game(user_grid, computer_grid, state):
     """Starts the game, gets input from user for move and adds a next move"""
     users_turn = True
-    while state.user_ships != 0 and state.computer_ships != 0:
+    while state.user_ships != 0 and state.com_ships != 0:
         if users_turn:
             move = get_move()
             state = shoot(computer_grid, move, True, state)
             users_turn = False
         else:
             print("computers turn...")
-            row = random.randint(1, 5)
-            col = random.randint(1, 5)
+            row = random.randint(1, state.rows)
+            col = random.randint(1, state.columns)
             move = str(row) + " " + str(col)
             state = shoot(user_grid, move, False, state)
             users_turn = True
@@ -115,7 +120,12 @@ def setup():
     user = input("Enter your user name: ")
     print("Good luck " + user)
 
-    state = State(1, 1, 1, 1,5)
+    print("Lets set up the grid!\n")
+    rows = int(input("How many rows: "))
+    cols = int(input("How many columns: "))
+    ships = int(input("How many ships: "))
+
+    state = State(ships, rows, cols)
     return state
 
 
